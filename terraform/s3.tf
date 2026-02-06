5beaa46dbb0b44a0860b9384a78a84b5998ff540
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-06 15:25:47 -0400

    infra(terraform): remote backend configure

---
+++ terraform/s3.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC        = "true"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  depends_on = [aws_s3_bucket.terraform_state]
+  
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "state_bucket"{
+  type        = string
+  default     = "my-terraform-state-bucket-liv28"
+  description = "Bucket with terraform state"
+}
