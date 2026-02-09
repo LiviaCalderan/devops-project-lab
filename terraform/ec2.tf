5cea6fa9d0fdbae8e84935a0aec02a06107448b0
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-09 08:17:43 -0400

    infra(terraform): AWS ami-data configure

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this" {
-  ami           = "ami-03ea746da1a2e36e7"
+  ami           = data.aws_ami.this.id
   instance_type = "t3.micro"
   associate_public_ip_address = true
 
@@ -8,4 +8,3 @@ resource "aws_instance" "this" {
     IAC = "true"
   }
 }
-
