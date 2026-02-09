8fceaae2641354d845ec460eea7f27f8a30979a8
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-09 08:56:28 -0400

    infra(terraform): AWS Security Group Configure - HTTP and HTTPS

---
+++ terraform/security_group.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "this" {
+    name        = "website-sg"
+    description = "Website security group allowing HTTP and HTTPS traffic"
+
+    tags = {
+        IAC = "true"
+    }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp" 
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp" 
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1" 
+}
+
