2ebcb34af99e4e181c9c0d58dea5d6516abc4333
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-09 09:24:47 -0400

    infra(terraform): IAM EC2 Role Configure and Instance role and sg attach

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,11 @@
 resource "aws_instance" "this" {
   ami           = data.aws_ami.this.id
   instance_type = "t3.micro"
+
   associate_public_ip_address = true
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_ssm_profile.name
+  vpc_security_group_ids = [aws_security_group.this.id]
 
   tags = {
     Name = "website-server"
