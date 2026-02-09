4ee333886b4748724c1b944df9ad8eb732ea0ff2
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-09 14:23:38 -0400

    fix(infra): VPC configure - ports and cidr

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -27,8 +27,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
   security_group_id = aws_security_group.this.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 0
-  to_port           = 0
   ip_protocol       = "-1"
 }
 
