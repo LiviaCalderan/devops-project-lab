22c50a73af4c9b6bc0b705582595afa59d9fc909
Lívia Calderan <liv_calderan@hotmail.com>
2026-02-10 15:50:44 -0400

    refactor(terraform): force_delete ci/cd option

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,6 +2,8 @@ resource "aws_ecr_repository" "this" {
   name                 = var.ecr_repository_name
   image_tag_mutability = "MUTABLE"
 
+  force_delete = true
+
   tags = merge(
     var.tags,
     {
